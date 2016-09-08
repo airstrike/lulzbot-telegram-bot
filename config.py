fefebb5c1b9697cfb695e5fb0fc9c1b4f547530e
Andre Terra <andreterra@gmail.com>
2016-09-07 23:07:45 -0400

    Added local.yaml config file to .gitignore

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 import yaml
 
-with open("config.yaml", 'r') as ymlfile:
-    config = yaml.load(ymlfile)
+try:
+    with open("local.yaml", 'r') as ymlfile:
+        config = yaml.load(ymlfile)
+except FileNotFoundError:
+    with open("config.yaml", 'r') as ymlfile:
+        config = yaml.load(ymlfile)
